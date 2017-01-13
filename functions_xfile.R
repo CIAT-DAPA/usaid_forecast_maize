b79c805f0e28eab4e388a9e5f7760ecdc7ed9d53
Jeikosd <jeison.mesa@correounivalle.edu.co>
2017-01-13 11:34:14 -0500

    update to initial conditions

--- functions_xfile.R
+++ functions_xfile.R
@@ -100,3 +100,87 @@ write_cultivars <- function(name_exp, information){
 
 # write_cultivars(proof, make_cultivars(CR, INGENO, CNAME))
 # close(proof)
+
+
+write_fields <- function(name_exp, information){
+  
+  # fields
+  cat("*FIELDS\n", file = name_exp)
+  cat("@L ID_FIELD WSTA....  FLSA  FLOB  FLDT  FLDD  FLDS  FLST SLTX  SLDP  ID_SOIL    FLNAME\n", file = name_exp)
+  for (i in 1:nrow(information)) {
+
+    cat(paste(sprintf("%2d",as.integer(information$L[i]))," ",sprintf("%-8s",information$ID_FIELD[i]),
+              " ",sprintf("%-8s",information$WSTA[i]),sprintf("%6d",as.integer(information$FLSA[i])),
+              sprintf("%6d",as.integer(information$FLOB[i])),sprintf("%6s",information$FLDT[i]),
+              sprintf("%6d",as.integer(information$FLDD[i])),sprintf("%6s",as.integer(information$FLDS[i])),
+              sprintf("%6d",as.integer(information$FLST[i]))," ",sprintf("%-4d",as.integer(information$SLTX[i])),
+              sprintf("%6d",as.integer(information$SLDP[i])),"  ",sprintf("%-10s",information$ID_SOIL[i])," ",
+              sprintf("%-12s",information$FLNAME[i]),"\n", sep=""),file=name_exp)
+
+  }
+
+  cat("\n", file = name_exp)
+  
+  
+  cat("@L ..........XCRD ...........YCRD .....ELEV .............AREA .SLEN .FLWR .SLAS FLHST FHDUR\n",file=name_exp)
+  # 
+  # for (i in 1:nrow(information)) { 
+  #   
+  #   
+  #   cat(paste(sprintf("%2d",as.integer(information$L[i]))," ",sprintf("%15.3f",information$XCRD[i])," ",
+  #             sprintf("%15.3f",information$YCRD[i])," ",sprintf("%9d",as.integer(information$ELEV[i]))," ",
+  #             sprintf("%17d",as.integer(information$AREA[i]))," ",sprintf("%5d",as.integer(information$SLEN[i]))," ",
+  #             sprintf("%5d",as.integer(information$FLWR[i]))," ",sprintf("%5d",as.integer(information$SLAS[i]))," ",
+  #             sprintf("%5d",as.integer(information$FLHST[i]))," ",sprintf("%5d",as.integer(information$FHDUR[i])),
+  #             "\n",sep=""),file=name_exp)
+  #   
+  #   
+  # }
+  
+  for (i in 1:nrow(information)) { 
+    
+    
+    cat(paste(sprintf("%2d",as.integer(information$L[i]))," ",sprintf("%14.d",information$XCRD[i])," ",
+              sprintf("%15.d",information$YCRD[i])," ",sprintf("%9d",as.integer(information$ELEV[i]))," ",
+              sprintf("%17d",as.integer(information$AREA[i]))," ",sprintf("%5d",as.integer(information$SLEN[i]))," ",
+              sprintf("%5d",as.integer(information$FLWR[i]))," ",sprintf("%5d",as.integer(information$SLAS[i]))," ",
+              sprintf("%5d",as.integer(information$FLHST[i]))," ",sprintf("%5d",as.integer(information$FHDUR[i])),
+              "\n",sep=""), file=name_exp)
+    
+    
+  }
+  
+  
+  cat("\n",file=name_exp)
+  
+}
+
+# write_fields(proof, make_fields(WSTA, ID_SOIL))
+# close(proof)
+
+
+write_IC <- function(name_exp, information){
+  
+  #initial conditions
+  cat("*INITIAL CONDITIONS\n",file=name_exp)
+  cat("@C   PCR ICDAT  ICRT  ICND  ICRN  ICRE  ICWD ICRES ICREN ICREP ICRIP ICRID ICNAME\n",file=name_exp)
+  
+  cat(paste(sprintf("%2d",as.integer(information$field$C))," ",sprintf("%5s",information$field$PCR),
+            " ",sprintf("%5s",information$field$ICDAT)," ",sprintf("%5d",as.integer(information$field$ICRT)),
+            " ",sprintf("%5d",as.integer(information$field$ICND))," ",sprintf("%5d",as.integer(information$field$ICRN)),
+            " ",sprintf("%5d",as.integer(information$field$ICRE))," ",sprintf("%5d",as.integer(information$field$ICWD)),
+            " ",sprintf("%5d",as.integer(information$field$ICRES))," ",sprintf("%5d",as.integer(information$field$ICREN)),
+            " ",sprintf("%5d",as.integer(information$field$ICREP))," ",sprintf("%5d",as.integer(information$field$ICRIP)),
+            " ",sprintf("%5d",as.integer(information$field$ICRID))," ",sprintf("%-12s",information$field$ICNAME),
+            "\n",sep=""),file=name_exp)
+  cat("@C  ICBL  SH2O  SNH4  SNO3\n",file=name_exp)
+
+
+  for (i in 1:nrow(information$values)) {
+    cat(paste(sprintf("%2d %5d %5d %5.2f %5.2f", 1, information$values[i, 'ICBL'], information$values[i, 'SH20'],
+                      information$values[i, 'SNH4'], information$values[i , 'SNO3'])), "\n", file = name_exp)
+  }
+  cat("\n",file=name_exp)
+}
+
+# write_IC(proof, make_IC(ICBL, SH20, SNH4, SNO3))
--- settings_xfile.R
+++ settings_xfile.R
@@ -18,8 +18,8 @@
 make_details <- function(DETAILS, PEOPLE){
   
   
-  general <- list(details,
-                  people,
+  general <- list(DETAILS = details,
+                  PEOPLE = people,
                   addres = "CIAT",
                   site = "CALI")
   
@@ -63,9 +63,9 @@ make_treatments <- function(IC, MI, MF, MH){
 # make_treatments(IC, MI, MF, MH)
 
 # Parameters
-CR <- 'MZ'    # Crop Code, you need to search this parameter for de manual DSSAT (its different by crop)
-INGENO <- 'CI0027' # Cultivar indentifier, this is the code for cultivar to run depend of crop
-CNAME <- 'PIO 30F35HRB_'  # Whatever code to identify the cultivar ran, maybe no too long string
+# CR <- 'MZ'    # Crop Code, you need to search this parameter for de manual DSSAT (its different by crop)
+# INGENO <- 'CI0027' # Cultivar indentifier, this is the code for cultivar to run depend of crop
+# CNAME <- 'PIO 30F35HRB_'  # Whatever code to identify the cultivar ran, maybe no too long string
 
 make_cultivars <- function(CR, INGENO, CNAME){
   
@@ -76,5 +76,91 @@ make_cultivars <- function(CR, INGENO, CNAME){
   
 }
 
-make_cultivars(CR, INGENO, CNAME)
+# make_cultivars(CR, INGENO, CNAME)
+
+
+# Parameters
+# WSTA <- 'CCBR1502' # Weather Station Code, its the same code to using in WTH file
+# ID_SOIL <- 'CCBuga0001' # Id soil to using in the SOIL.SOl
+
+
+
+make_fields <- function(WSTA, ID_SOIL){
+  
+  
+  # *FIELDS
+  # @L ID_FIELD WSTA....  FLSA  FLOB  FLDT  FLDD  FLDS  FLST SLTX  SLDP  ID_SOIL    FLNAME
+  #  1 -99      CCBR1502   -99   -99 DR000   -99   -99     0 SL      30  CCBuga0001 Calibracion
+  # 
+  fields <- data.frame(L = 1, ID_FIELD = "USAID", WSTA, FLSA = -99, FLOB = -99, FLDT = "DR000",
+                               FLDD = -99, FLDS = -99, FLST = -99, SLTX = -99, SLDP = -99, ID_SOIL,
+                               FLNAME = "FIELD01", XCRD = -99, YCRD = -99, ELEV = -99, AREA = -99, SLEN=-99,
+                               FLWR = -99, SLAS = -99, FLHST = -99, FHDUR=-99)
+  
+
+  return(fields)
+  
+}
+
+# make_fields(WSTA, ID_SOIL) 
+
+
+
+# Parameters
+
+
+# Initial Conditions
+# 1 ) you are going to use initial conditions for default (soil characteristics) (see code for running DSSAT on BID project)
+# 2 ) you need to use lower limit or upper limit (para la siembra, esto mas que todo se utiliza cuando se desea sembrar automaticamente)
+# 3 ) you are goint to give the information to make Initial Conditions
+# Por ahora se utilizará condiciones iniciales dadas por el usuario
+
+## data frame to Initial Conditions
+# ICDAT the same from PDATE (planting date)
+# *INITIAL CONDITIONS
+# @C   PCR ICDAT  ICRT  ICND  ICRN  ICRE  ICWD ICRES ICREN ICREP ICRIP ICRID ICNAME
+# 1    MZ 80085   -99     0     1     1   -99     0     0     0   100    15 -99
+
+
+# ICBL <- c(25, 45, 95)
+# SH20 <- -99
+# SNH4 <- c(4.2, 4.4, 4.5)    # estas variables se pueden investigar cuales utilizar cuando no se tiene condiciones iniciales
+# SNO3 <- c(11.9, 12.4, 7.6)  # estas variables se pueden investigar cuales utilizar cuando no se tiene condiciones iniciales
+# ICDAT <- -99 #  for now
+
+make_IC <- function(ICBL, SH20, SNH4, SNO3){
+  
+  IC_field <- data.frame(C = 1, PCR = 'MZ', ICDAT = -99, ICRT = -99, ICND = -99, ICRN = -99, ICRE = -99,
+                                       ICWD = -99, ICRES = -99, ICREN = -99, ICREP = -99, ICRIP = -99, ICRID = -99,
+                                       ICNAME = -99)
+  
+  IC_values <- data.frame(C= rep(1,length(SNH4)),ICBL, SH20,SNH4,
+                          SNO3)
+  
+
+  
+  initial_conditions <- list(field = IC_field, values = IC_values)
+  
+  return(initial_conditions)
+  
+}
+
+
+# make_IC(ICBL, SH20, SNH4, SNO3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
