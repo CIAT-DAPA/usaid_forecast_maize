9dfb1322c916960c83e25a5fc221ef34ddc3a551
Jeikosd <jeison.mesa@correounivalle.edu.co>
2017-02-05 19:17:34 -0500

    add date to make .WTH with the real year and day

--- make_wth.R
+++ make_wth.R
@@ -188,7 +188,7 @@ make_date <- function(data){
   require(tidyverse)
   require(lubridate)
   
-  # data <- climate_list_df[[1]]
+  # data <- read_csv(climate_list[[1]])
   current_year <- Sys.Date() %>%
                     year()
   
@@ -199,10 +199,14 @@ make_date <- function(data){
   frcast_date <- seq(init_frcast,
                   end_frcast, by = '1 day')
   
-  ## Add to generate variable to dssat_date
+  # is possible to eliminate some variables?
   
   data <-  tbl_df(data.frame(data, frcast_date)) %>%
-              mutate(julian_day = yday(frcast_date), year_2 = substr(year(frcast_date), 3, 4))
+              mutate(julian_day = yday(frcast_date),
+                     year_2 = as.numeric(substr(year(frcast_date), 3, 4)), 
+                     date_dssat = )
+  
+  mapply(date_for_dssat, data$year_2, data$julian_day)
   
   return(data)
 }
@@ -211,16 +215,13 @@ make_date <- function(data){
 climate_list_df <- lapply(climate_list, read_csv) %>%
                       lapply(make_date)
 
-# z <- lapply(climate_list_df, make_date)
-
-# suppressWarnings(expr)
 
 ##  generate all .WTH files to run DSSAT 
 ## Proof
-# data <- z[[1]] 
-# out_dir <- 'D:/CIAT/USAID/DSSAT/multiple_runs/R-DSSATv4.6/Runs/'
-# lat <- -99
-# long <- -99
+data <- climate_list_df[[1]]
+out_dir <- 'D:/CIAT/USAID/DSSAT/multiple_runs/R-DSSATv4.6/Proof_run/'
+lat <- -99
+long <- -99
 
 # climate_df <- read_csv(paste0(path, station, '.csv'))%>%
 #   mutate(date = dmy(paste(day, month, year, sep = '/'))) %>%
@@ -247,8 +248,10 @@ climate_list_df <- lapply(climate_list, read_csv) %>%
 ## Date = day_dssat   # fecha para la construccion del Julian day necesario para que DSSAT entienda la informacion climatica
 
 
+mapply(date_for_dssat, data$year_2, data$julian_day)
 
 
+date_for_dssat()
 
 out_dir <- 'D:/CIAT/USAID/DSSAT/multiple_runs/R-DSSATv4.6/Proof_run/'
 
@@ -257,17 +260,12 @@ out_dir <- 'D:/CIAT/USAID/DSSAT/multiple_runs/R-DSSATv4.6/Proof_run/'
 
 # climate_list_df <- lapply(climate_list_df, mutate, date_dssat = frcast_date)
 
-
-climate_list_df[[1]] <- climate_list_df[[1]]%>%
-  # mutate(date = dmy(paste(day, month, year, sep = '/'))) %>%
-                          mutate(julian_day = yday(frcast_date), year_2 = substr(year(frcast_date), 3, 4))
-
-
 ## make a function to do this
 date_dssat <- 0
-for(i in 1:dim(z[[1]])[1]){
 
-  date_dssat[i] <- date_for_dssat(z[[1]]$year_2[i], z[[1]]$julian_day[i])
+for(i in 1:dim(data)[1]){
+
+  date_dssat[i] <- date_for_dssat(data[[1]]$year_2[i],data[[1]]$julian_day[i])
 
 }
 
